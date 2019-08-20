a92fa8776d78d80d0c295ac607f78119dee19153
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-08-19 22:21:00 -0300

    Acerto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "...",
-    authDomain: "project.firebaseapp.com",
-    databaseURL: "https://project.firebaseio.com",
-    projectId: "project",
-    storageBucket: "project.appspot.com",
-    messagingSenderId: "..."
+    apiKey: "AIzaSyCOly8cVb8-zb6G5TpvtgiBIvrp-ON_POY",
+    authDomain: "angular-52901.firebaseapp.com",
+    databaseURL: "https://angular-52901.firebaseio.com",
+    projectId: "angular-52901",
+    storageBucket: "angular-52901.appspot.com",
+    messagingSenderId: "66044788405",
+    appId: "1:66044788405:web:f2627f5f026d3fb3"
   }
 };
