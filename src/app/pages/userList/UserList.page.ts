18225278b744801de0b420591b4fe67f70481de0
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-08-21 16:02:28 -0300

    Correcoes Pipe Ordernação

--- src/app/pages/userList/UserList.page.ts
+++ src/app/pages/userList/UserList.page.ts
@@ -2,39 +2,56 @@ import { Component } from '@angular/core';
 import { UsersService } from '../../services/Users.service';
 
 @Component({
-    templateUrl: './UserList.page.html',
-    styleUrls: ['./UserList.page.css']
+  templateUrl: './UserList.page.html',
+  styleUrls: ['./UserList.page.css']
 })
 
 export class UserListPage {
 
-    public dados = [];
+  public dados = [];
 
-    public filterBy: string = '';
+  public filterBy: string = '';
 
-    constructor(
-         private usersService: UsersService
-    ) { }
+  public nameButton: string = '';
 
-    ngOnInit() {
-        this.list();
-    }
+  public textOrderBy: string = 'ASC';
 
-    private list() {
-        this.dados = [];
-        this.usersService.getUsers()
-            .subscribe((data: any) => {
-                Object.keys(data)
-                .forEach((index) => {
-                  //debugger;
-                    this.dados.push(data[index].payload.doc.data());
-                });
-            });
-    }
+  // public orderBy: [];
 
+  constructor(
+    private usersService: UsersService
+  ) { }
 
-    setFilterBy(event: any) {
-      this.filterBy = event.target.value;
+  ngOnInit() {
+    this.list();
   }
+
+  private list() {
+    this.dados = [];
+    this.usersService.getUsers()
+      .subscribe((data: any) => {
+        Object.keys(data)
+          .forEach((index) => {
+            //debugger;
+            this.dados.push(data[index].payload.doc.data());
+          });
+      });
+  }
+
+
+  setFilterBy(event: any) {
+    this.filterBy = event.target.value;
+  }
+
+  setOrderBy(event: any) {
+    this.nameButton = event.target.innerHTML;
+    if (this.nameButton === 'ASC') {
+      this.nameButton = this.textOrderBy = 'DESC';
+    } else {
+      this.nameButton = this.textOrderBy = 'ASC';
+    }
+    event.target.innerHTML = this.nameButton;
+  }
+
 
 }
--- src/app/pipes/order.pipe.ts
+++ src/app/pipes/order.pipe.ts
@@ -1,12 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { orderBy } from 'lodash';
 
 @Pipe({
   name: 'order'
 })
 export class OrderPipe implements PipeTransform {
 
-  transform(items: any, filterBy: string) {
-    return null;
+  transform(items: any, nameButton: string) {
+    console.info(nameButton);
+
+    if (!items) return [];
+    if (!nameButton) return items;
+
+    if (nameButton === 'ASC') {
+      return orderBy(items, 'nome', 'asc');
+    } else {
+      return orderBy(items, 'nome', 'desc');
+    }
+
+
   }
 
 }
